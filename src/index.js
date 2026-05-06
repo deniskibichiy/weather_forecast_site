7077763d6bfe500034eadbe5c2039222017c15b9
Denis Kibichiy <deniskibichiy@outlook.com>
2026-05-06 10:21:10 +0300

    implement function to hit API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./style.css";
+
+
+const API_KEY = "CWKX7FE9ENF5WTUYVUPRQEYKL";
+
+async function fetchData(location) {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`;
+
+    try {
+        const request = await fetch(url);
+
+        if (!request.ok) {
+            throw new Error(`Response status: ${request.status}`);
+        }
+        const response = await request.json();
+        return response;
+    }
+    catch (e) {
+        console.error(e);
+    }
+}
+
+
+
+
+
+
