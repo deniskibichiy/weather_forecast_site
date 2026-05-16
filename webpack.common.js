c121bab0a1b556e0375fa35b0d5a8618ca74aea4
Denis Kibichiy <deniskibichiy@outlook.com>
2026-05-16 18:07:42 +0300

    switch build output to docs for github pages

--- webpack.common.js
+++ webpack.common.js
@@ -5,9 +5,9 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     clean: true,
-    //publicPath: "./",
+    publicPath: "./",
   },
   devtool: "eval-source-map",
   devServer: {
